4fff097656939413a589c5651011b77b7b4e487a
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-15 01:35:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const byteSize = (str) => {
-  // write your code here
+  // write your code her
+	let d=new Blob([str],{ type: "text/plain" });
+	// console.log(d.size);=
+	return d.size;
 };
 
-// Do not change the code below
+// Do not change the code below/
 const str = prompt("Enter some string.");
 alert(byteSize(str));
